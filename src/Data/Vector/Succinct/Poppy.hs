ac1a2dec07f256895125b05a9f0cf83e9debd8ea
Morita Yasuaki <zousandazou@gmail.com>
2013-11-07 18:24:31 +0900

    implement poppy only rank

--- src/Data/Vector/Succinct/Poppy.hs
+++ src/Data/Vector/Succinct/Poppy.hs
@@ -40,6 +40,21 @@ class SucBV sbv where
 data Poppy = Poppy {-# UNPACK #-} !Int !(Array Bit) 
                 !(U.Vector Word64) 
                 !(U.Vector Word64)
+                deriving (Eq,Ord,Show,Read)
+
+
+instance SucBV Poppy where
+    isobv = _Poppy
+    rank (Bit True) n (Poppy len (V_Bit _ ws) ubs lbs) = c + c'
+      where (iw,ow) = n `divMod` 64
+            up = iw `div` ubsize
+            (lp,lo) = iw `divMod` lbsize
+            f 0 i = i .&. (2^32 -1)
+            f n i = ((i `unsafeShiftR` ((n-1) * 10 + 32)) .&. (2^10-1)) + f (n-1) i
+            c = popCount ((ws U.! iw) .&. (2 ^ ow - 1))
+            c' = fromIntegral ((ubs U.! up) + f lo (lbs U.! lp))
+    rank _ n p = n - rank (Bit True) n p
+    select b n poppy@(Poppy len _ _ _) = search (\i -> n < rank b i poppy) 0 (len-1)
 
 _Poppy :: Iso' Poppy (Array Bit)
 _Poppy = iso (\(Poppy _ v _ _) -> v) $ f
@@ -56,42 +71,29 @@ bbsize = 2^9 `div` 64
 lbsize = 2^11 `div` 64
 ubsize = 2^32 `div` 64
 
-
--- |
--- >>> showTable . build $ U.fromList ((take 45 (repeat 1)) ++ (take 12 (repeat 0)))
-
-showTable :: (U.Vector Word64,U.Vector Word64) -> [Word64]
-showTable (ub,lb) = [ p | i <- U.toList lb, p <- [f 0 i, f 1 i, f 2 i, f 3 i]]
-    where f 0 i = i .&. (2^32 -1)
-          f n i = ((i `unsafeShiftR` ((n-1) * 10 + 32)) .&. (2^10-1)) + f (n-1) i
-
-
-build :: U.Vector Word64 -> (U.Vector Word64,U.Vector Word64)
-build vs = runST $ do 
-  sumbb <- newSTRef (0 :: Word64)
+buildPoppy :: U.Vector Word64 -> (U.Vector Word64,U.Vector Word64)
+buildPoppy vs = runST $ do 
   sumlb <- newSTRef (0 :: Word64)
   sumub <- newSTRef (0 :: Word64)
   uindv <- MU.new ublen :: ST s (MU.STVector s Word64)
   lindv <- MU.new lblen :: ST s (MU.STVector s Word64)
-  let loop i = when (i < len) $ do
-                 let (ui,uo,li,lo,bi,bo) = sep i
-                 when (uo == 0) $ do
-                   ub <- readSTRef sumub
-                   MU.write uindv ui ub
-                   writeSTRef sumlb 0
-                 when (lo == 0) $ do
-                   lb <- readSTRef sumlb
-                   MU.write lindv li lb
-                   modifySTRef' sumub (+ lb)
-                 when (bo == 0) $ do
-                   when (bi > 0 ) $ do
-                     d <- MU.read lindv li
-                     bb <- readSTRef sumbb
-                     MU.write lindv li (d + (bb `unsafeShiftL` ((bi - 1) * 10 + 32)))
-                     modifySTRef' sumlb (+ bb)
-                     writeSTRef sumbb 0
-                 modifySTRef' sumbb (+ pop i)
-                 loop (i+1)
+  let loop i = do when (i < len) $ do
+                    let (ui,uo,li,lo,bi,_) = sep i
+                    when (uo == 0) $ do
+                      ub <- readSTRef sumub
+                      MU.write uindv ui ub
+                      writeSTRef sumlb 0
+                    when (lo == 0) $ do
+                      lb <- readSTRef sumlb
+                      MU.write lindv li lb
+                      modifySTRef' sumub (+ lb)
+                    let r = (safeSlice i bbsize vs) :: U.Vector Word64
+                        bbsum = fromIntegral . U.sum . U.map popCount $ r
+                    modifySTRef' sumlb (+ bbsum)
+                    when (bi /= (lbsize `div` bbsize) - 1 ) $ do
+                      d <- MU.read lindv li
+                      MU.write lindv li (d + (bbsum `unsafeShiftL` (bi * 10 + 32)))
+                    loop (i+bbsize)
   loop 0
   uindv' <- U.freeze uindv
   lindv' <- U.freeze lindv
@@ -100,30 +102,21 @@ build vs = runST $ do
         ublen = len `divCeil` ubsize
         lblen = len `divCeil` lbsize
         sep i = let (ui,uo) = i `divMod` ubsize
-                    (li,lo) = i `divMod` ubsize
+                    (li,lo) = i `divMod` lbsize
                     (bi,bo) = lo `divMod` bbsize
                 in (ui,uo,li,lo,bi,bo) :: (Int,Int,Int,Int,Int,Int)
-        pop i = fromIntegral $ popCount (vs U.! i)
-
-
-
-bbsum :: Int -> U.Vector Word64 -> U.Vector Word64
-bbsum n vs = G.unfoldr chunk vs
-  where chunk xs | U.null xs = Nothing
-                 | otherwise = let (a,b) = (U.splitAt n xs) in Just (U.sum . U.map (fromIntegral . popCount) $ a,b)
+        safeSlice i s vs = U.take s (U.drop i vs)
 
 
 divCeil :: Int -> Int -> Int
 divCeil n b = (n + b - 1) `div` b
 
-
-buildPoppy :: U.Vector Word64 -> (U.Vector Word64, U.Vector Word64)
-buildPoppy ws = undefined -- todo
  
 
 
 -- |
--- >>> search (\n->n^2 > 16) 0 100
+-- >>> search (\n->n^2 >= 16) 0 5
+-- 4
 search :: Integral n => (n -> Bool) -> n -> n -> n
 search p l h | l == h = l
              | p m = search p l m
@@ -134,6 +127,7 @@ search p l h | l == h = l
 --
 -- sampling width = 8192           32 bits par min 2^13 bits
 --                                 3.12500 + max 0.390625% additional space
+
 
 data CSPoppy = CSPoppy {-# UNPACK #-} !Int !(Array Bit) 
                   !(U.Vector Word64) 
